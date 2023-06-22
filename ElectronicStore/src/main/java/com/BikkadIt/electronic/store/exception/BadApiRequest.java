f772fce699e4eed981a197afafd31bd8f4e79381
ITsomesh <itisomesh@gmail.com>
2023-06-22 18:14:46 +0530

    project code up to BadApiRequest.

---
+++ ElectronicStore/src/main/java/com/BikkadIt/electronic/store/exception/BadApiRequest.java
@@ -0,0 +1,13 @@
+package com.BikkadIt.electronic.store.exception;
+
+public class BadApiRequest extends RuntimeException{
+
+    public BadApiRequest(String message){
+      super(message) ;
+
+    }
+    public BadApiRequest(){
+        super("Bad Request !!");
+
+    }
+}
