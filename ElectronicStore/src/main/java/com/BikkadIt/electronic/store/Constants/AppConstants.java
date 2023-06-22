dcbde19ebb0b4fd6a28469f17edae79a2d811f42
ITsomesh <itisomesh@gmail.com>
2023-06-22 18:11:39 +0530

    project code up to AppConstants.

---
+++ ElectronicStore/src/main/java/com/BikkadIt/electronic/store/Constants/AppConstants.java
@@ -0,0 +1,22 @@
+package com.BikkadIt.electronic.store.Constants;
+
+public class AppConstants {
+
+    //USER CONSTANTS
+    public static  final String USER_DELETE="User Delete Successful !!";
+    public static  final String NOT_FOUND="User Not Found with This Id !!";
+    public static final String IMAGE=" Image uploaded Successfully !! ";
+
+    //PRODUCT CONSTANTS
+    public static final String PRODUCT_DELETE="Product Deleted By ProductId";
+    public static final String PRODUCT_NOT_FOUND="Product Not Found With This Id!!";
+    public static final String PRODUCT_IMAGE=" Image uploaded Successfully !! ";
+
+    //CATEGORY CONSTANTS
+    public static final String CATEGORY_DELETE="Product Deleted By ProductId";
+    public static final String CATEGORY_NOT_FOUND="Product Not Found With This Id!!";
+    public static final String CATEGORY_IMAGE=" Image uploaded Successfully !! ";
+
+    public static final String CATEGORY_ID= "Category Id Incorrect !! ";
+
+}
