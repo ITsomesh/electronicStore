5510fb53c9ee237dbd621077954325be47018278
ITsomesh <itisomesh@gmail.com>
2023-07-09 16:14:29 +0530

    project code up to ProductRepository.

--- ElectronicStore/src/main/java/com/BikkadIt/electronic/store/repository/ProductRepository.java
+++ ElectronicStore/src/main/java/com/BikkadIt/electronic/store/repository/ProductRepository.java
@@ -1,16 +1,16 @@
 package com.BikkadIt.electronic.store.repository;
 
+import com.BikkadIt.electronic.store.entities.Category;
 import com.BikkadIt.electronic.store.entities.Product;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.awt.print.Pageable;
-import java.util.List;
 
 public interface  ProductRepository extends JpaRepository<Product,String> {
 
     //Page<Product>findByTitleContaining(String subTitle,Pageable pageable);
 
     //Page<Product> findByLiveTrue(Pageable pageable);
+    Page<Product>findByCategory(Category category, Pageable pageable);
 
 }
